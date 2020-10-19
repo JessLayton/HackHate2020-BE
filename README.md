2c4fea6a67bb23ae37fdccb6f76695e612cf811f
Joshua Halls <joshuah40@googlemail.com>
2020-10-19 14:29:07 +0100

    api skelton

--- README.md
+++ README.md
@@ -4,5 +4,26 @@ Backend code for the DDPO data collection/visualation project for [HackHate 2020
 
 ## Installation
 
-To install for development, run `pip -e .`. Run `pip install -r requirements.txt` to also install the [black](https://black.readthedocs.io/en/stable/)
-code formatter.
+Setup the virtual enviroment
+```bash
+python -m venv venv
+```
+
+Install the python dependencies 
+```bash
+pip install -r requirements.txt
+```
+
+Run the app
+```bash
+export FLASK_APP=dataColl
+
+flask run
+```
+
+## Test
+The app contains a test API call. Flask will by default be hosted on `http://localhost:5000/`. The API call is available at the route `/time`. You can view this in a broswer by going to `http://localhost:5000/time` or using curl:
+
+```bash
+curl http://localhost:5000/time
+```
---
+++ dataColl/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask
+from datetime import datetime
+
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'dataColl.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a test route 
+    @app.route('/time')
+    def get_current_time():
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        return {'time': dt_string}
+
+    from . import db
+    db.init_app(app)
+
+    return app
