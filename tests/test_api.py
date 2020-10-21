c8d111d36aaaaf7f5a2bddd2a6677805e8e3765e
Dylan Evans <dylanlee.evans95@gmail.com>
2020-10-21 20:03:02 +0100

    Set up unit testing
    - Add test for getDDPOs endpoint
    - Add conftest.py with various fixtures
    - Add config options for testing to setup.cfg
    - Add mock data in data.sql

---
+++ tests/test_api.py
@@ -0,0 +1,14 @@
+"""test_api.py - Test API endpoints"""
+import pytest
+from dataColl import api
+
+
+def test_getDDPOs(client):
+    """Test getDDPOs endpoint."""
+    response = client.get('/api/getDDPOs')
+    json = response.get_json()
+    assert json == [
+        {'id': 1, 'name': 'Foo DDPO'},
+        {'id': 2, 'name': 'Bar DDPO'},
+        {'id': 3, 'name': 'Baz DDPO'}
+    ]
