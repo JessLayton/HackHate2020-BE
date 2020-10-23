8350c44e76f97acf46e88806c8c8eaa8197c1597
Joshua Halls <joshuah40@googlemail.com>
2020-10-23 22:16:33 +0100

    minor change2

--- json_test.py
+++ json_test.py
@@ -4,21 +4,12 @@ with open("example.json") as f:
     form_data = json.load(f)
 
 ## Test POST API JSON
-import json
 import requests
-
 r = requests.post('http://localhost:5000/api/form', json=form_data)
 
 
-start_date = form_data['time_period']['start']
-end_date = form_data['time_period']['end']
-name_ddpo = form_data['name_ddpo']
-borough_covered_str = ', '.join([str(elem) for elem in form_data['borough_covered']])
-
 import sqlite3
-
 db_file = "instance/dataColl.sqlite"
-
 db = sqlite3.connect(db_file, detect_types=sqlite3.PARSE_DECLTYPES)
 db.row_factory = sqlite3.Row
 
@@ -34,9 +25,15 @@ form = db.execute(
 for i in form:
     print(list(i))
 
-def process_form():
+def process_form(form_data: dict):
     start_date = form_data['time_period']['start']
     end_date = form_data['time_period']['end']
     name_ddpo = form_data['name_ddpo']
     borough_covered_str = ', '.join([str(elem) for elem in form_data['borough_covered']])
     
+
+    
+    print("done")
+
+
+process_form(form_data)
