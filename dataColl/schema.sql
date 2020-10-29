08a99aa2337de11f11b7bb621dba41576f7e5d78
Stephen <srerribekibele@gmail.com>
2020-10-29 21:31:38 +0000

    Added crud statements to the schema.sql

--- dataColl/schema.sql
+++ dataColl/schema.sql
@@ -1,14 +1,16 @@
 -- Drop existing objects
+Drop TABLE IF EXISTS Response;
+DROP TABLE IF EXISTS Ethnicity;
+DROP TABLE IF EXISTS NoResponseReason;
+DROP TABLE IF EXISTS Gender;
+DROP TABLE IF EXISTS EnquiryType;
+DROP TABLE IF EXISTS SupportType;
+DROP TABLE IF EXISTS SupportAgeCategory;
+DROP TABLE IF EXISTS SexualOrientation;
+DROP TABLE IF EXISTS Sex;
+DROP TABLE IF EXISTS CaseRelatedCategory;
+DROP TABLE IF EXISTS Borough;
 DROP TABLE IF EXISTS Organisation;
-DROP TABLE IF EXISTS form_data;
-DROP TABLE IF EXISTS enquires;
-DROP TABLE IF EXISTS support_provided;
-DROP TABLE IF EXISTS cases_not_police_report;
-DROP TABLE IF EXISTS support_age;
-DROP TABLE IF EXISTS ethnicity;
-DROP TABLE IF EXISTS gender;
-DROP TABLE IF EXISTS cases_related_to;
-
 -- Create tables
 CREATE TABLE Organisation (
     -- A DDPO
@@ -16,91 +18,128 @@ CREATE TABLE Organisation (
     Name TEXT NOT NULL
 );
 
-CREATE TABLE form_data (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    start_date date,
-    end_date date,
-    name_ddpo TEXT,
-    borough_covered TEXT,
-    ddpo_short_paragraph TEXT,
-    casestudy_highlighting_emotional_impact TEXT,
-    casestudy_highlighting_positive_outcome TEXT
-);
-
-CREATE TABLE enquires (
-    dhc INTEGER,
-    police INTEGER,
-    not_police INTEGER,
-    self_referrals INTEGER,
-    police_referrals INTEGER
-);
-
-CREATE TABLE support_provided (
-    hate_crime INTEGER,
-    emotional INTEGER,
-    general INTEGER,
-    other INTEGER
-);
-
-CREATE TABLE cases_not_police_report (
-    lack_evidence INTEGER,
-    cd_not_trust_police INTEGER,
-    cd_police_not_believe INTEGER,
-    cd_afraid INTEGER,
-    cd_abuse_stop INTEGER,
-    cd_talk INTEGER,
-    other INTEGER
-);
-
-CREATE TABLE support_age (
-    under_17 INTEGER,
-    eighteen_to_65 INTEGER,
-    sixtyfive_plus INTEGER
-);
-
-CREATE TABLE ethnicity (
-    white_eng_plus INTEGER,
-    white_irish INTEGER, 
-    white_traveller INTEGER,
-    white_other INTEGER,
-    mixed_caribbean INTEGER,
-    mixed_african INTEGER,
-    mixed_asian INTEGER,
-    mixed_multiple INTEGER,
-    asian_indian INTEGER,
-    asian_pakistani INTEGER,
-    asian_bang INTEGER,
-    asian_chinese INTEGER,
-    asian_other INTEGER,
-    black_african INTEGER,
-    black_caribbean INTEGER,
-    black_other INTEGER,
-    other_arab INTEGER,
-    other INTEGER
-);
+CREATE TABLE Response(
+    ID INTEGER PRIMARY KEY, 
+    Year INTEGER,
+    Quarter INTEGER,
+    FOREIGN KEY (Organisation_ID) REFERENCES Organisation(Id),
+    CasesReferredToPolice INTEGER,
+    CasesNotReferredToPolice INTEGER,
+    HateCrimeReferrals INTEGER,
+    FreeTextResponse TEXT,
+    CaseStudyEmotionalImpact TEXT,
+    CaseStudyPositiveOutcome TEXT
     
-CREATE TABLE gender (
-    cis_male INTEGER,
-    cis_female INTEGER,
-    male INTEGER,
-    female INTEGER,
-    hetro INTEGER,
-    homo INTEGER,
-    bi INTEGER,
-    lesbian INTEGER,
-    other INTEGER
 );
 
-CREATE TABLE cases_related_to (
-    covid INTEGER,
-    neighbour INTEGER,
-    friend_carer INTEGER,
-    home INTEGER,
-    domestic_violence INTEGER,
-    care_home INTEGER,
-    verbal INTEGER,
-    physical INTEGER,
-    street INTEGER,
-    public_transport INTEGER,
-    other INTEGER
+CREATE TABLE Ethnicity(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+    
+);
+CREATE TABLE NoReportReason(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+
+);
+CREATE TABLE Gender(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+CREATE TABLE EnquiryType(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+); 
+CREATE TABLE SupportType(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+CREATE TABLE SupportAgeCategory(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+CREATE TABLE SexualOrientation(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+CREATE TABLE Sex(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+CREATE TABLE CaseRelatedCategory(
+    ID INTEGER PRIMARY KEY,
+    Description TEXT
+);
+
+CREATE TABLE Borough(
+    ID INTEGER PRIMARY KEY,
+    NAME TEXT
+);
+
+CREATE TABLE Organisation(
+    ID INTEGER PRIMARY KEY,
+    NAME TEXT
+);
+
+CREATE TABLE EthnicityTotal(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY(Ethnicity_ID) REFERENCES Ethnicity(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE NoReportReasonTotal(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (NoReportResponse_ID) REFERENCES NoReportReason(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE GenderTotal(
+    FOREIGN KEY(Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY(Gender_ID) REFERENCES Gender(ID),
+    TOTAL INTEGER
+
+);
+
+CREATE TABLE EnquiryTypeTotal(
+    FOREIGN KEY(Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY(EnquiryType_ID) REFERENCES EnquiryType(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE SupportTypeTotal(
+    FOREIGN KEY(Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (SupportType_ID) REFERENCES SupportType(ID),
+    TOTAL INTEGER
+
+);
+
+CREATE TABLE SupportAgeCategoryTotals(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (SupportAgeCategory_ID) REFERENCES SupportAgeCategory(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE SexualOrientationTotal(
+    FOREIGN KEY(Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (SexualOrientation_ID) REFERENCES SexualOrientation(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE SexTotal(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (Sex_ID) REFERENCES Sex(ID),
+    TOTAL INTEGER
+);
+
+CREATE TABLE CaseRelatedCategoryTotal(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (CaseRelatedCategory_ID) REFERENCES CaseRelatedCategory(ID),
+    TOTAL INTEGER
+
+);
+
+CREATE TABLE ResponseBorough(
+    FOREIGN KEY (Response_ID) REFERENCES Response(ID),
+    FOREIGN KEY (Borough_ID) REFERENCES Borough(ID)
+
 );
