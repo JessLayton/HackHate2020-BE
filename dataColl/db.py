bc319c012adafcff5b8bd816bbf7409ff4789c94
Dylan Evans <dylanlee.evans95@gmail.com>
2020-10-31 17:49:45 +0000

    Complete form endpoint

--- dataColl/db.py
+++ dataColl/db.py
@@ -1,6 +1,5 @@
 """db.py - Database helper functions."""
 import sqlite3
-from sqlite3 import OperationalError
 
 import click
 from flask import current_app, g
@@ -53,57 +52,111 @@ def query_db(query, args=(), one=False):
     return (rv[0] if rv else None) if one else rv
 
 
+def insert_response(form_data):
+    """
+    Insert the relevant data from `form_data` into the Reponse
+    table. 
+
+    Return the Id of the created Response.
+    """
+    db = get_db()
+    org_id_query = 'SELECT Id FROM Organisation WHERE Name = ?'
+    org = query_db(org_id_query, (form_data['name_ddpo'],), one=True)
+    if org is not None:
+        org_id = org['Id']
+    else:
+        raise ValueError('Organisation does not exist')
+
+    cur = db.execute(
+        '''
+        INSERT INTO Response (
+            Year,
+            Quarter,
+            OrganisationId,
+            CasesReferredToPolice,
+            CasesNotReferredToPolice,
+            HateCrimeReferrals,
+            FreeTextResponse,
+            CaseStudyEmotionalImpact,
+            CaseStudyPositiveOutcome
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+        ''',
+        (
+            form_data['year'],
+            form_data['quarter'],
+            org_id,
+            form_data['cases_reported'],
+            form_data['cases_not_reported'],
+            form_data['hate_crime_cases'],
+            form_data['ddpo_short_paragraph'],
+            form_data['casestudy_impact'],
+            form_data['casestudy_outcome'],
+        )
+    )
+    return cur.lastrowid
+
+
+def insert_totals(response_id, form_data):
+    """
+    Insert totals from form data into the relevant tables.
+    """
+    # Map category tables to the form data
+    KEY_TABLE = {
+        'ethnicity': 'Ethnicity',
+        'cases_not_police_report': 'NoReportReason',
+        'cases_related_to': 'CaseRelatedCategory',
+        'gender': 'Gender',
+        'impairments': 'Impairment',
+        'orientation': 'SexualOrientation',
+        'referrals': 'ReferralType',
+        'sex': 'Sex',
+        'support_age': 'SupportAgeCategory',
+        'support_provided': 'SupportType'
+    }
+    db = get_db()
+
+    for key, category_table in KEY_TABLE.items():
+        totals = form_data[key]
+        total_table = category_table + 'Total'
+        id_column = category_table + 'Id'
+        for json_key, total in totals.items():
+            query = f'''
+            INSERT INTO {total_table} (ResponseId, {id_column}, Total)
+            SELECT {response_id}, Id, {total}
+            FROM {category_table}
+            WHERE JSONKey = ?;
+            '''
+            db.execute(query, (json_key,))
+
+
 def process_form(form_data: dict) -> tuple:
-    
-    top_level_fields = {}
-    top_level_fields["start_date"] = form_data['time_period']['start']
-    top_level_fields["end_date"] = form_data['time_period']['end']
-    top_level_fields["name_ddpo"] = form_data['name_ddpo']
-    top_level_fields["borough_covered"] = ', '.join([str(elem) for elem in form_data['borough_covered']])
-    top_level_fields["ddpo_short_paragraph"] = form_data["ddpo_short_paragraph"]
-    top_level_fields["casestudy_highlighting_emotional_impact"] = form_data["casestudy_highlighting_emotional_impact"]
-    top_level_fields["casestudy_highlighting_positive_outcome"] = form_data["casestudy_highlighting_positive_outcome"]
-
-    fields = top_level_fields.keys()
-    question_marks = ", ".join("?" * len(fields))
-    column_str = ", ".join([str(elem) for elem in fields])
-
-    sql = f"INSERT into form_data ({column_str}) VALUES ({question_marks})"
-
-    try:
-        db = get_db()
-        db.execute(sql, ([top_level_fields[key] for key in fields]))
-        db.commit()
-        return None
-    except Exception as e:
-        error = str(e.__class__) + " : " + repr(e)
-        return error
-        
-
-def process_question(form_data: dict, question: str) -> tuple:
     """
+    Insert form response data into the database.
 
+    Return details of any errors that occur in the process.
     """
-
-    #if question == "time_period":
-     #   return None
-    if not isinstance(form_data[question], dict):
-        return None
-    
-    keys = form_data[question].keys()
-    question_marks = ", ".join("?" * len(keys))
-    keys_str = ", ".join([str(elem) for elem in form_data[question].keys()])
-
-    sql = f"INSERT into {question} ({keys_str}) VALUES ({question_marks})"
-
+    # Insert into Response table
     try:
-        db = get_db()
-        db.execute(sql, ([form_data[question][key] for key in keys]))
-        db.commit()
-        return None
-    except OperationalError as e:
-        error = str(e.__class__) + " : " + repr(e)
-        return error
+        response_id = insert_response(form_data)
+    except ValueError as e:
+        raise e
+
+    # Insert into totals tables
+    insert_totals(response_id, form_data)
+
+    # Insert into ResponseBorough
+    boroughs = form_data['boroughs_covered']
+    borough_insert = '''
+    INSERT INTO ResponseBorough (ResponseId, BoroughId)
+    SELECT ?, Id
+    FROM Borough
+    WHERE Name = ?;
+    '''
+
+    db = get_db()
+    db.executemany(borough_insert, ((response_id, b) for b in boroughs))
+    db.commit()
 
 
 def init_db():
